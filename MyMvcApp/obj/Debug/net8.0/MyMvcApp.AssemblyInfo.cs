0db04a410c3947457f14efae7f1a5109d96ed93f
kairui1012 <TP074684@mail.apu.edu.my>
2026-03-17 14:18:29 +0800

    readme.md add

--- MyMvcApp/obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ MyMvcApp/obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4f5e33417123370b185bae7e44aabbd9f255bed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7513c723bc548163631c635669205f8a8721e4cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
