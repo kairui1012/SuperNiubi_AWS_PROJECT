8f5f6a84be8f87ad571db0cbc76e4ff5e6e67a82
yeoyulelele <153090665+yeoyulelele@users.noreply.github.com>
2026-04-20 12:34:34 +0800

    index page created

--- MyMvcApp/Controllers/TenantController.cs
+++ MyMvcApp/Controllers/TenantController.cs
@@ -1,13 +1,26 @@
+using Amazon.Extensions.CognitoAuthentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyMvcApp.Controllers
+
 {
     [Authorize] // Ensures only logged-in users can reach this page
     public class TenantController : Controller
     {
-        public IActionResult Tenant()
+        private readonly UserManager<CognitoUser> _userManager;
+
+        public TenantController(UserManager<CognitoUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Tenant()
+        {
+            var email = User.FindFirst(System.Security.Claims.ClaimTypes.Email)?.Value
+                    ?? User.Identity?.Name;
+            ViewBag.TenantEmail = email;
             return View();
         }
     }
