9f1294586ca618a95e90db262abcffb1fc4a760e
yeoyulelele <153090665+yeoyulelele@users.noreply.github.com>
2026-04-24 14:18:03 +0800

    Version 2

--- MyMvcApp/obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ MyMvcApp/obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b198170aeeeea982c2d7283e5b7289ffdac9a69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
