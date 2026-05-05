a4e3ec8a3a39d8e4dcc294764f49ac201557530b
ElvinLim13 <elvin.lim.522@gmail.com>
2026-05-05 21:41:47 +0800

    Update latest changes

--- MyMvcApp/wwwroot/js/site.js
+++ MyMvcApp/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener('wheel', (event) => {
+    const target = event.target;
+
+    if (!(target instanceof HTMLInputElement)) {
+        return;
+    }
+
+    if (target.type !== 'number' || document.activeElement !== target) {
+        return;
+    }
+
+    if (!target.closest('.landlord-cloud-skin')) {
+        return;
+    }
+
+    event.preventDefault();
+}, { passive: false });
